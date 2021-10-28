5997286a616e35762a21b9d57b96cc19ec45df59
Tom <ashandrikov@gmail.com>
2021-10-28 10:00:17 +0300

    classList & event delegation

--- js/hw08.js
+++ js/hw08.js
@@ -1,3 +1,30 @@
-const btns = document.querySelectorAll('button');
+const btns = document.querySelectorAll('button'),
+    wrapper = document.getElementById('first');
 
-console.log(btns[0].classList.length);
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(0));
+// console.log(btns[1].classList.add('red'));
+// console.log(btns[0].classList.remove('blue'));
+// console.log(btns[0].classList.toggle('blue'));
+
+// if (btns[1].classList.contains('red')) {
+//     console.log('red');
+// } 
+
+// btns[0].addEventListener('click', () => {
+//     if (!btns[1].classList.contains('red')) {
+//         btns[1].classList.add('red');
+//     } else {
+//         btns[1].classList.remove('red');
+//     }
+// });
+
+wrapper.addEventListener('click', (e) => {
+    if (e.target && e.target.matches("button.blue")) {
+        console.dir(e.target);
+    } 
+});
+
+const btn = document.createElement('button');
+btn.classList.add('blue');
+wrapper.append(btn);
