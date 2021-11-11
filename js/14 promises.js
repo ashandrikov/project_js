c4917d3cdad9e6787bb45b5544bee6101bd88b5d
Tom <ashandrikov@gmail.com>
2021-11-11 18:05:33 +0300

    streams

---
+++ js/14 promises.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const req = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        console.log('Подготовка данных...');
+
+        const product = {
+            name: 'TV',
+            price: 2000
+        };
+
+        resolve(product);
+    }, 2000);
+});
+
+req.then((product) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            product.status = 'order';
+            resolve(product);
+        }, 2000);
+    });
+}).then(data => {
+    data.modify = true;
+    return data;
+}).then((data) => {
+    console.log(data);
+}).catch(() => {
+    console.errpr('error');
+}).finally(() => {
+    console.log('finally');
+});
+
+// const test = time => {
+//     return new Promise(resolve => {
+//         setTimeout(() => resolve(), time);
+//     });
+// };
+
+// Promise.all([test(1000), test(2000)]).then(() => {
+//     console.log('all');
+// });
+// Promise.race([test(1000), test(2000)]).then(() => {
+//     console.log('any');
+// });
